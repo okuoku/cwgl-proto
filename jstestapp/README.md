40c721bccee2af59e69fb3f273947b974cb06329
okuoku <mjt@cltn.org>
2020-12-27 19:50:43 +0900

    testapp: Merge emtestapp

---
+++ jstestapp/README.md
@@ -0,0 +1,40 @@
+About
+=====
+
+TBD
+
+Prebuilt binaries
+=================
+
+## App1
+
+ImGUI sample. https://github.com/ocornut/imgui/tree/master/examples/example_emscripten_opengl3
+
+The MIT License (MIT)
+
+Copyright (c) 2014-2020 Omar Cornut
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## App2
+
+Development build of UTS2 sample https://github.com/unity3d-jp/UnityChanToonShaderVer2_Project
+
+Unity-Chan Toon Shader 2.0 is provided under the Unity-Chan License 2.0 terms.
+© Unity Technologies Japan/UCL
---
+++ jstestapp/index.js
@@ -0,0 +1,284 @@
+
+const process = require("process");
+const fs = require("fs");
+const GL = require("gl");
+const PNG = require("pngjs").PNG;
+const indexedDB = require("fake-indexeddb");
+const performance = require('perf_hooks').performance;
+
+const nav = {};
+const doc = {};
+const wnd = {};
+
+wnd.document = doc;
+wnd.navigator = nav;
+
+function sleep(ms){
+    return new Promise((res) => setTimeout(res, ms));
+}
+
+let g_ctx = null;
+const pixels0 = new Uint8Array(1920 * 1080 * 4);
+const pixels1 = new Uint8Array(1920 * 1080 * 4);
+let flip_fb = 0;
+let shots = 0;
+
+function update_screenshot(){
+    let pixels = false;
+    if(flip_fb == 0){
+        pixels = pixels0;
+        flip_fb = 1;
+    }else{
+        pixels = pixels1;
+        flip_fb = 0;
+    }
+    //console.log("Pre", g_ctx.getError());
+    g_ctx.readPixels(0, 0, 1280, 720, g_ctx.RGBA, g_ctx.UNSIGNED_BYTE, pixels);
+    //console.log("Post", g_ctx.getError());
+    shots++;
+
+    //console.log("Shot", shots);
+    let pngout = PNG.sync.write({width: 1280, height: 720, data: pixels});
+    fs.writeFileSync("out" + shots.toString() + ".png", pngout);
+    console.log("Save", shots);
+}
+
+// FakeFetch
+
+function fake_fetch(path, opts) {
+    console.log("Fake fetch", path, opts);
+    if(path == "example_emscripten_opengl3.wasm"){
+        return new Promise(ret => {
+            ret({
+                ok: true,
+                arrayBuffer: function(){
+                    let bin = fs.readFileSync("app/example_emscripten_opengl3.wasm");
+                    console.log(bin);
+                    return new Promise(res => {
+                        res(bin);
+                    });
+                }
+            });
+        });
+    }else if(path == "build.wasm"){
+        // Remap for Unity
+        return new Promise(ret => {
+            ret({
+                ok: true,
+                arrayBuffer: function(){
+                    let bin = fs.readFileSync("app2/gltest2.wasm");
+                    console.log(bin);
+                    return new Promise(res => {
+                        res(bin);
+                    });
+                }
+            });
+        });
+    }else{
+        return null;
+    }
+}
+
+// Emscripten patches
+
+function fake_aEL(typ, lis, usecapture){
+    console.log("Add Event Listender", typ, lis, usecapture);
+}
+
+function fake_rEL(typ){
+    console.log("Remove Event Listender", typ);
+}
+
+const my_canvas = {
+    style: {
+        cursor: "bogus"
+    },
+    getBoundingClientRect: function(){
+        return {
+            top: 0,
+            bottom: 720,
+            left: 0,
+            right: 1280,
+            x: 0,
+            y: 0,
+            width: 1280,
+            height: 720
+        };
+    },
+    addEventListener: fake_aEL,
+    removeEventListener: fake_rEL,
+    getContext: function(type,attr){
+        console.log("Draw context", type, attr);
+        if(type == "webgl"){
+            g_ctx = GL(1280,720,attr);
+            g_ctx.canvas = this;
+            return g_ctx;
+        }
+        return null;
+    }
+
+};
+
+const my_module = {
+    locateFile: function (path, scriptDirectory) {
+        return path;
+    },
+    canvas: my_canvas,
+    // For Unity
+    preRun: [],
+    postRun: [],
+    SystemInfo: {
+        hasWebGL: true,
+        gpu: "Dummy GPU"
+    },
+    webglContextAttributes: {
+        premultipliedAlpha: false,
+        preserveDrawingBuffer: false
+    },
+    setInterval: setInterval,
+    // app2 application (Unity)
+    dataUrl: "Build/gltest2.data",
+    frameworkUrl: "Build/gltest2.framework.js",
+    codeUrl: "Build/gltest2.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "WebGLUTSTest",
+    productVersion: "0.1",
+
+};
+
+const my_screen = {
+    width: 1280,
+    height: 720
+};
+
+
+wnd.navigator.getGamepads = function(){
+    console.log("GetGamepads");
+    return [];
+}
+
+wnd.requestAnimationFrame = function(cb){
+    console.log("rAF");
+    process.nextTick(async function(){
+        await sleep(100);
+        const now = performance.now();
+        console.log("RAF", now);
+        cb(now);
+        update_screenshot();
+    });
+    return 99.99;
+}
+
+wnd.indexedDB = indexedDB;
+
+function fake_settimeout(cb, ms){
+    console.log("sTO", cb, ms);
+    process.nextTick(async function(){
+        await sleep(ms);
+        const now = performance.now();
+        console.log("FRAME", now);
+        cb();
+        update_screenshot();
+    });
+}
+
+
+// FakeDom
+
+function fake_queryselector(tgt){
+    console.log("querySelector", tgt);
+    if(tgt == "#canvas"){
+        return my_canvas;
+    }else{
+        return null;
+    }
+}
+
+wnd.document.querySelector = fake_queryselector;
+wnd.document.addEventListener = fake_aEL; // specialHTMLTargets[1]
+wnd.addEventListener = fake_aEL; // specialHTMLTargets[2]
+wnd.navigator.userAgent = "bogus";
+wnd.navigator.appVersion = "bogus";
+
+wnd.document.URL = "";
+
+// Boot
+global.my_window = wnd;
+global.my_fetch = fake_fetch;
+global.my_doc = wnd.document;
+global.my_module = my_module;
+global.my_screen = my_screen;
+global.fake_settimeout = fake_settimeout;
+
+function boot(){ // Emscripten plain
+    const bootstrap = fs.readFileSync("app/example_emscripten_opengl3.js", "utf8");
+    let window = global.my_window;
+    let navigator = window.navigator;
+    let fetch = global.my_fetch;
+    let document = global.my_doc;
+    var Module = global.my_module;
+    let screen = global.my_screen;
+    let setTimeout = global.fake_settimeout;
+    eval(bootstrap);
+}
+
+/*
+function boot(){ // Unity
+    const bootstrap = fs.readFileSync("app2/gltest2.framework.js", "utf8");
+    // Unity preload
+    function cb_injectdata(data) { // From Unity 2020.1
+        let view = new DataView(data.buffer, data.byteOffset, data.byteLength);
+        let pos = 0;
+        let prefix = "UnityWebData1.0\0";
+        pos += prefix.length;
+        let headerSize = view.getUint32(pos, true); 
+        pos += 4;
+        while (pos < headerSize) {
+            let offset = view.getUint32(pos, true); 
+            pos += 4;
+            let size = view.getUint32(pos, true); 
+            pos += 4;
+            let pathLength = view.getUint32(pos, true); 
+            pos += 4;
+            let path = String.fromCharCode.apply(null, data.subarray(pos, pos + pathLength)); 
+            pos += pathLength;
+            for (var folder = 0, folderNext = path.indexOf("/", folder) + 1 ;
+                 folderNext > 0; 
+                 folder = folderNext, folderNext = path.indexOf("/", folder) + 1){
+                console.log("Inject Dir", path);
+                Module.FS_createPath(path.substring(0, folder), path.substring(folder, folderNext - 1), true, true);
+            }
+            console.log("Inject File", path);
+            Module.FS_createDataFile(path, null, data.subarray(offset, offset + size), true, true, true);
+        }
+    }
+
+    function injectdata(){
+        let data = fs.readFileSync("app2/gltest2.data");
+        cb_injectdata(data);
+    }
+
+    global.my_module.preRun.push(injectdata);
+
+    function fake_alert(obj){
+        console.log("ALERT", obj);
+    }
+
+    let window = global.my_window;
+    let navigator = window.navigator;
+    let fetch = global.my_fetch;
+    let document = global.my_doc;
+    var Module = global.my_module;
+    let screen = global.my_screen;
+    let setTimeout = global.fake_settimeout;
+    let alert = fake_alert;
+    eval(bootstrap + "\n\n global.initfunc = unityFramework;");
+
+    let init = global.initfunc;
+    init(global.my_module);
+}
+*/
+
+boot();
+
