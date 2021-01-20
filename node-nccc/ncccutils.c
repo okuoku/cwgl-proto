20b21c209785d80fd49ef4e4d132afd50cadf46d
okuoku <mjt@cltn.org>
2021-01-21 02:50:36 +0900

    node-nccc: Add ncccutils

---
+++ node-nccc/ncccutils.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#define UTILLIB_API __declspec(dllexport)
+
+#include <stdlib.h>
+
+typedef void (*nccc_call_t)(const uint64_t* in, uint64_t* out);
+
+UTILLIB_API void
+util_peek_u64(const uint64_t* in, uint64_t* out){
+    // [addr] => [v]
+    const uint64_t* addr = (uintptr_t)in[0];
+    const uint64_t d = *addr;
+    out[0] = d;
+}
+
+UTILLIB_API void
+util_peek_u32(const uint64_t* in, uint64_t* out){
+    // [addr] => [v]
+    const uint32_t* addr = (uintptr_t)in[0];
+    const uint32_t d = *addr;
+    out[0] = d;
+}
+
+UTILLIB_API void
+util_poke_u64(const uint64_t* in, uint64_t* out){
+    // [addr v] => []
+    uint64_t* const dest = (uintptr_t)in[0];
+    (void) out;
+    *dest = in[0];
+}
+
+UTILLIB_API void
+util_poke_u32(const uint64_t* in, uint64_t* out){
+    // [addr v] => []
+    uint32_t* const dest = (uintptr_t)in[0];
+    (void) out;
+    *dest = in[0];
+}
+
+UTILLIB_API void
+util_malloc(const uint64_t* in, uint64_t* out){
+    // [size] => [addr]
+    const size_t size = in[0];
+    const void* addr = malloc(size);
+    out[0] = addr;
+}
