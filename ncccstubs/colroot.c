e05b527955f0aa85d01efd74d8c455ef8e624024
okuoku <mjt@cltn.org>
2021-01-30 19:25:17 +0900

    ncccstub: Init

---
+++ ncccstubs/colroot.c
@@ -0,0 +1,34 @@
+#define EXPORT __declspec(dllexport)
+
+#include <stdlib.h>
+#include <stdint.h>
+
+void lib_yfrm_dispatch(const uint64_t* in, uint64_t* out);
+
+EXPORT void 
+yfrm_nccc_root_00(const uint64_t* in, uint64_t* out){
+    switch(in[0]){
+        case 0:
+            switch(in[1]){
+                case 0:
+                    if(in[2] == 1){
+                        out[0] = 0; /* buildid */
+                        out[1] = 0; /* buildid */
+                        out[2] = 1; /* libcount */
+                        out[3] = 1; /* maxversion */
+                    }else{
+                        abort();
+                    }
+                    return;
+            }
+            return;
+        case 1 /* lib */:
+            switch(in[1]){
+                case 0:
+                    lib_yfrm_dispatch(&in[2], out);
+                    return;
+                default:
+                    abort();
+            }
+    }
+}
---
+++ ncccstubs/stublead.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "stubprefix_yfrm.h"
+#include "stubdata_yfrm.h"
+#include "stubcode.inc.c"
