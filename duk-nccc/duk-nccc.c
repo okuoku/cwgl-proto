0347b6423e0426b1b5c0fff15884ddcde6f62e02
okuoku <mjt@cltn.org>
2021-02-17 19:09:49 +0900

    duk-nccc: Implement more

--- duk-nccc/duk-nccc.c
+++ duk-nccc/duk-nccc.c
@@ -1,3 +1,4 @@
+// FIXME: We use global-stash[pointer] to keep backreference
 #include <string.h>
 #include <stdlib.h>
 #include "duktape.h"
@@ -7,10 +8,6 @@
 #else
 #include <alloca.h>
 #endif
-
-#include <setjmp.h>
-static jmp_buf g_jmpbuf;
-static int calldepth = 0;
 
 static void
 value_in(duk_context* ctx, char type, uint64_t vin){
@@ -146,7 +143,7 @@ do_finalize(duk_context* ctx){
 }
 
 static duk_ret_t
-nccc_wrap_pointer(duk_context* ctx){
+wrap_pointer(duk_context* ctx){
     // [ptr dispatch ctx arg] => ptr
     //   [dispatch,ctx] = [arg ptr] => []
     finalizer_params_t* params;
@@ -167,4 +164,275 @@ nccc_wrap_pointer(duk_context* ctx){
     return 1;
 }
 
+struct cb_params_s {
+    char* intypes;
+    char* outtypes;
+    size_t incount;
+    size_t outcount;
+    /* For nccc_call */
+    uint64_t addr;
+    uint64_t dispatch;
+    /* For nccc_cb */
+    duk_context* ctx;
+};
 
+typedef struct cb_params_s cb_params_t;
+
+static duk_ret_t
+destroy_cb_ctx(duk_context* ctx){
+    // [params] => undefined
+    uint64_t ptr;
+    value_out(ctx, &ptr, 'p', 0);
+    /* Remove params from Global stash */
+    duk_push_global_stash(ctx);
+    duk_del_prop_index(ctx, -1, ptr);
+    free((cb_params_t*)(uintptr_t)ptr);
+    return 0;
+}
+
+static duk_ret_t
+nccc_call_trampoline(duk_context* ctx){
+    int i;
+    uint64_t* inbuf;
+    uint64_t* outbuf;
+    nccc_call_t fn;
+    cb_params_t* params;
+    duk_push_current_function(ctx);
+    (void)duk_get_prop_index(ctx, -1, 0);
+    params = duk_require_pointer(ctx, -1);
+
+    if(params->dispatch){
+        inbuf = alloca(sizeof(uint64_t)*(params->incount+1));
+    }else{
+        inbuf = alloca(sizeof(uint64_t)*params->incount);
+    }
+    outbuf = alloca(sizeof(uint64_t)*params->outcount);
+    /* Setup arguments */
+    if(params->dispatch){
+        inbuf[0] = params->addr;
+        for(i=0;i!=params->incount;i++){
+            value_out(ctx,&inbuf[i+1],params->intypes[i],i);
+        }
+    }else{
+        for(i=0;i!=params->incount;i++){
+            value_out(ctx,&inbuf[i],params->intypes[i],i);
+        }
+    }
+
+    /* Call */
+    if(params->dispatch){
+        fn = (nccc_call_t)params->dispatch;
+    }else{
+        fn = (nccc_call_t)params->addr;
+    }
+
+    /* No need to handle exception here.
+     * Duktape uses setjmp/longjmp for exceptions */
+
+    fn(inbuf, outbuf);
+
+    if(params->outcount == 0){
+       return 0;
+   }else if(params->outcount == 1){
+       /* Push a value and return */
+       value_in(ctx, params->outtypes[0], outbuf[0]);
+       return 1;
+   }else{ /* Return as an array */
+       duk_push_array(ctx);
+       for(i=0;i!=params->outcount;i++){
+           value_in(ctx, params->outtypes[i], outbuf[i]);
+           duk_put_prop_index(ctx, -2, i);
+       }
+       return 1;
+   }
+}
+
+static void
+free_cb_params(cb_params_t* p){
+    free(p->intypes);
+    free(p->outtypes);
+    free(p);
+}
+
+static duk_ret_t
+do_free_nccc_call(duk_context* ctx){
+    cb_params_t* params;
+
+    (void)duk_get_prop_index(ctx, -1, 0);
+    params = duk_require_pointer(ctx, -1);
+    free_cb_params(params);
+
+    return 0;
+}
+
+static const char*
+dukstrdup(duk_context* ctx, duk_idx_t v){
+    size_t len;
+    const char* buf;
+    char* p;
+
+    buf = duk_require_lstring(ctx, v, &len);
+    p = malloc(len+1);
+    memcpy(p, buf, len);
+    p[len] = 0;
+    return (const char*)p;
+}
+
+static duk_ret_t
+make_nccc_call(duk_context* ctx){
+    // [debugstring, dispatch, addr, intypes, outtypes] => closure
+    cb_params_t* params;
+
+    // FIXME: debugstring
+
+    params = malloc(sizeof(cb_params_t)); /* FIXME: Allocate this to stack first */
+    params->intypes = dukstrdup(ctx, 3);
+    params->outtypes = dukstrdup(ctx, 4);
+    (void)duk_require_lstring(ctx, 3, &params->incount);
+    (void)duk_require_lstring(ctx, 4, &params->outcount);
+    value_out(ctx, &params->dispatch, 'p', 1);
+    value_out(ctx, &params->addr, 'p', 2);
+    params->ctx = NULL;
+
+    duk_push_c_function(ctx, nccc_call_trampoline, params->incount);
+    duk_push_pointer(ctx, params);
+    duk_put_prop_index(ctx, -2, 0);
+
+    duk_push_c_function(ctx, do_free_nccc_call, 1);
+    duk_set_finalizer(ctx, -2);
+
+    return 1;
+}
+
+static void
+nccc_cb_dispatcher(const uint64_t* in, uint64_t* out){
+    // [params inaddr] => (dispatch)
+    // Native to JavaScript bridge
+
+    duk_context* ctx;
+    cb_params_t* params = (cb_params_t*)(uintptr_t)in[0];
+    const uint64_t* in_next = (uint64_t*)(uintptr_t)in[1];
+    int i;
+    char outtypes[33];
+    int outcount;
+
+    ctx = params->ctx;
+    /* Function */
+    duk_push_global_stash(ctx);
+    (void)duk_get_prop_index(ctx, -1, in[0]);
+
+    /* Args */
+    for(i=0;i!=params->incount;i++){
+        value_in(ctx, params->intypes[i], in_next[i]);
+    }
+
+    // Capture ctx data so we can free ctx inside callback
+    outcount = params->outcount;
+    strncpy(outtypes, params->outtypes, 32);
+    outtypes[32] = 0;
+
+    duk_call(ctx, params->incount);
+    
+    // Parse and fill output
+    if(outcount){
+        if(duk_is_array(ctx, -1)){
+            for(i=0;i!=outcount;i++){
+                (void)duk_get_prop_index(ctx, -1, i);
+                value_out(ctx, &out[i], outtypes[i], -1);
+                duk_pop(ctx);
+            }
+        }else{
+            value_out(ctx, &out[0], outtypes[0], -1);
+        }
+    }
+}
+
+static duk_ret_t
+make_nccc_cb(duk_context* ctx){
+    // [cb, intypes, outtypes] => [dispatch ctx]
+    //      __types:
+    //         'i' : 32bit int
+    //         'l' : 64bit int
+    //         'p' : pointer
+    //         'f' : float
+    //         'd' : double
+
+    cb_params_t* params;
+
+    params = malloc(sizeof(cb_params_t)); /* FIXME: Allocate this to stack first */
+    params->intypes = dukstrdup(ctx, 1);
+    params->outtypes = dukstrdup(ctx, 2);
+    (void)duk_require_lstring(ctx, 1, &params->incount);
+    (void)duk_require_lstring(ctx, 2, &params->outcount);
+    params->dispatch = 0;
+    params->addr = 0;
+    params->ctx = ctx;
+
+    /* retain callback */
+    duk_push_global_stash(ctx);
+    duk_dup(ctx, 0);
+    (void)duk_put_prop_index(ctx, -2, (uintptr_t)params);
+
+    /* Output */
+    duk_push_array(ctx);
+    duk_push_pointer(ctx, nccc_cb_dispatcher);
+    (void)duk_put_prop_index(ctx, -2, 0);
+    duk_push_pointer(ctx, params);
+    (void)duk_put_prop_index(ctx, -2, 1);
+
+    return 1;
+}
+
+#define EXPAND_CORELIB(x) \
+    x(util_rawcall) \
+    x(util_peek_u64) \
+    x(util_peek_u32) \
+    x(util_poke_u64) \
+    x(util_poke_u32) \
+    x(util_malloc) \
+    x(util_free) \
+    x(util_ptraddr) \
+    x(dlfcn_open) \
+    x(dlfcn_get)
+
+#define ITR_PROTOTYPE(nam) \
+    void nam(const uint64_t* in, uint64_t* out);
+
+EXPAND_CORELIB(ITR_PROTOTYPE)
+
+static void
+set_corelib(duk_context* ctx, const char* name, void* sym){
+    duk_push_pointer(ctx, sym);
+    (void)duk_put_prop_string(ctx, -2, name);
+}
+
+static void /* push a value */
+create_corelib(duk_context* ctx){
+    duk_push_object(ctx);
+#define ITR_SETCORELIB(sym) set_corelib(ctx, #sym, sym);
+    EXPAND_CORELIB(ITR_SETCORELIB)
+#undef ITR_SETCORELIB
+}
+
+static void
+set_export(duk_context* ctx, const char* name, void* sym, int argc){
+    duk_push_c_function(ctx, sym, argc);
+    (void)duk_put_prop_string(ctx, -2, name);
+}
+
+void
+dukregisternccc(duk_context* ctx){
+    duk_push_global_object(ctx);
+    duk_push_object(ctx);
+#define REGISTER_EXPORT(sym,argc) set_export(ctx, #sym, sym, argc);
+    REGISTER_EXPORT(make_nccc_cb,3);
+    REGISTER_EXPORT(make_nccc_call,5);
+    REGISTER_EXPORT(destroy_cb_ctx,1);
+    REGISTER_EXPORT(wrap_pointer,4);
+
+    create_corelib(ctx);
+    (void)duk_put_prop_string(ctx, -2, "corelib");
+
+    (void)duk_put_prop_string(ctx, -2, "NCCC");
+    duk_pop(ctx);
+}
--- duk-nccc/yuniduk.c
+++ duk-nccc/yuniduk.c
@@ -3,8 +3,44 @@
 #include <stdio.h>
 #include "duktape.h"
 
+#include "duk_print_alert.h"
+
+#define BOOTSTRAP_FILE (SCRIPTROOT "/bootstrap.js")
+
+void dukregisternccc(duk_context* ctx);
+
+void
+dukdebugwrite(long level, const char* file, long line, const char* func, const char* msg){
+    fprintf(stderr, "D%ld %s:%d (%s): %s\n", level, file, line, func, msg);
+}
+
+static void
+dukload(duk_context* ctx, const char* filename, int flags){
+    long siz;
+    FILE* fp;
+    char* buf;
+    fp = fopen(filename, "r");
+    fseek(fp,0,SEEK_END);
+    siz = ftell(fp);
+    fseek(fp,0,SEEK_SET);
+    buf = malloc(siz+1);
+    fread(buf,1,siz,fp);
+    buf[siz] = 0;
+
+    duk_push_string(ctx, filename);
+    duk_compile_string_filename(ctx, flags, buf);
+    free(buf);
+    duk_call(ctx, 0);
+    duk_pop(ctx);
+}
+
 int main(int argc, char *argv[]) {
     duk_context *ctx = duk_create_heap_default();
+    duk_print_alert_init(ctx, 0);
+    dukregisternccc(ctx);
+    duk_eval_string(ctx, "print(JSON.stringify(NCCC));");
+    duk_eval_string(ctx, "print(NCCC.corelib.util_rawcall);");
+    dukload(ctx, BOOTSTRAP_FILE, DUK_COMPILE_EVAL);
     duk_destroy_heap(ctx);
     return 0;
 }
